4d60e41ec1493e9e2f038a2848db21500b011e5f
Rudraksh pandey <rudrakshpandey8@gmail.com>
2026-03-19 12:57:05 +0530

    Added Git pages ddeploy script

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Portfolio-V2/',  // ← add this, must match your GitHub repo name exactly
 })
